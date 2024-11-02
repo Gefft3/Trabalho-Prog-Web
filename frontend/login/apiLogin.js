af7e4a32e56d15f82958047dd9265c5e39f86144
Geffte Caetano <gefft3@gmail.com>
2024-11-01 22:06:02 -0400

    Add: home template
    Refactor: userController.js and userRoutes.js

--- frontend/login/apiLogin.js
+++ frontend/login/apiLogin.js
@@ -1,4 +1,10 @@
-async function loginUser() {
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('loginForm').addEventListener('submit', loginUser);
+}); 
+
+async function loginUser(event) {
+    event.preventDefault();
+
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
@@ -16,13 +22,13 @@ async function loginUser() {
         const data = await response.json();
 
         if (response.ok) {
-            alert('deu certo');
-            console.log('deu certo');
+            console.log('Logou!');
+            window.location.href = 'http://127.0.0.1:3001/frontend/home/home.html'
         } else {
-            alert(`deu errado, pq: ${data.message}`);
+            alert(`${data.message}`);
         }
     } catch (error) {
-        console.error('deu errado', error);
-        alert('deu errado');
+        console.error(error);
+        alert(error);
     }
 }
---
+++ frontend/register/apiRegister.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('registerForm').addEventListener('submit', registerUser);
+});
+
+async function registerUser(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    console.log(name, email, password);
+    
+    try {
+        const response = await fetch('http://localhost:3000/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert('Registro bem-sucedido!');
+            console.log('Usuário cadastrado!');
+            window.location.href = 'http://127.0.0.1:3001/frontend/login/login.html';
+        } else {
+            alert(`Erro: ${data.message}`);
+        }
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
+};
