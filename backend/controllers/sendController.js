ab70d2b19495c5d504a6f963dc624fd8633dded1
catarwnalud <catarinaludmilanf@gmail.com>
2024-11-25 16:15:15 -0400

    Send email test

--- backend/controllers/sendController.js
+++ backend/controllers/sendController.js
@@ -10,21 +10,19 @@ const sendEmail = async (req, res) => {
             return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
         }
 
-        // Verificando se o remetente existe no banco
+        // Verifica se o remetente existe no banco
         const senderQuery = await pool.query('SELECT id FROM users WHERE email = $1', [senderEmail]);
         if (senderQuery.rowCount === 0) {
             return res.status(404).json({ message: 'Remetente não encontrado.' });
         }
-        //const senderId = senderQuery.rows[0].id;
 
-        // Verificando se o destinatário existe no banco
+        // Verifica se o destinatário existe no banco
         const recipientQuery = await pool.query('SELECT id FROM users WHERE email = $1', [recipientEmail]);
         if (recipientQuery.rowCount === 0) {
             return res.status(404).json({ message: 'Destinatário não encontrado.' });
         }
-        //const recipientId = recipientQuery.rows[0].id;
 
-        // Inserindo o e-mail no banco de dados
+        // Insere o e-mail no banco de dados
         const result = await pool.query(
             `INSERT INTO messages (subject, content, send_date, status, sender_email, recipient_email)
              VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
